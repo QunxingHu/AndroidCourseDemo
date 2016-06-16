375527f22e4eace759641763a2dbd5be3bddd9f3
keenezhu <waterzhj@ustc.edu.cn>
2016-06-16 13:42:21 +0800

    Initial commit

---
+++ coursedemo/src/main/java/com/example/coursedemo/DataModel.java
@@ -0,0 +1,24 @@
+package com.example.coursedemo;
+
+import java.io.Serializable;
+
+/**
+ * this file belongs to package com.example.coursedemo of My Application
+ * created at 14:58,07,2016
+ *
+ * @author keene
+ * @version 1.0
+ */
+public class DataModel implements Serializable{
+
+    private String data;
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+}
